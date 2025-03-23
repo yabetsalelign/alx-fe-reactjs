3baa2e7a72c1c8bea5f8ace67e5926c2f516d579
yabetsalelign <149236920+yabetsalelign@users.noreply.github.com>
2025-03-23 12:15:26 -0700

    Task 0,1,2 & 3

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <Routes>
   <Route path="/" element={<HomePage />} />
   <Route path="/recipe/:id" element={<RecipeDetail />} />
-  <Route path="/add-recipe" element={<AddRecipeForm />} /> {/* New route for the form */}
+  <Route path="/add-recipe" element={<AddRecipeForm />} /> {/* New route for the form   */}
 </Routes>
 
     </Router>
